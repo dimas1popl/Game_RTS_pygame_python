a9f30b987889d1414a71e844b79aae1cbf4c7a26
dimas1popl <dididimas738@gmail.com>
2026-01-01 20:49:08 +0300

    News windows

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import pygame
 
 pygame.init()
+
+Width = 1600
+Height = 900
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+blue = (0,0,255)
+green = (0,255,0)
+pink = (255,0,255)
+yellow = (255,255,0)
+cyan = (0,255,255)
+magenta = (255,0,255)
+
+display = pygame.display.set_mode((Width, Height))
+
+On_Game_True = True
+while On_Game_True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            On_Game_True = False
+            quit()
+
+    display.fill(cyan)
+
+    pygame.display.flip()
+
+
